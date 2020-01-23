20c406d018c0fad9f0627b29fc50510cba4c2d0b
Galya777 <58034004+Galya777@users.noreply.github.com>
2020-01-23 17:13:14 +0200

    Is there a repeating digit?

---
+++ easytask10.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+bool equal_digits(int number, int mask) {
+	if (number == 0) {
+		return false;
+	}
+		int lastdigit = 1 << (number % 10);
+		if (mask&lastdigit) {
+			return true;
+		}
+		return equal_digits(number / 10, mask | lastdigit);
+}
+int main()
+{
+	const int MASK = 0;
+	int number;
+	cin >> number;
+	if (equal_digits(number,MASK)) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+
+
+	return 0;
+}
+
