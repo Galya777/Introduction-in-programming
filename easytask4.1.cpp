0f1c0c5349c52eb997681dfbe28481369536f9f9
Galya777 <58034004+Galya777@users.noreply.github.com>
2020-03-29 20:39:35 +0300

    Add files via upload

---
+++ easytask4.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+bool simple(unsigned int p) {
+	if (p < 2) {
+		return false;
+	} for (int i = 2; i <= p / 2; i++) {
+		if (p % i == 0) {
+			return false;
+		}
+	} return true;
+	}
+int main()
+{
+	int N;
+	cin >> N;
+	int m;
+	int n;
+	if (N % 2 == 0) {
+	for (m = 0; m <= N / 2; m++) {
+	for (n = N; n >= N / 2; n--) {
+	if ((simple(m) == true) && (simple(n) == true)) {
+		m + n;
+	if (m + n == N) {
+		cout << '{' << m << ',' << n << '}'<<" ";
+	} 			
+	}
+	}
+		}
+	} else {
+		cout << "Are you joking with me, Euler? This number is not even! ";
+	}
+	return 0;
+}
+
