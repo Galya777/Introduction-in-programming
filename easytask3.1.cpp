c5964e49ee0f72a5725c5a300b6ad6ff2cf48f9a
Galya777 <58034004+Galya777@users.noreply.github.com>
2020-03-30 16:47:03 +0300

    Add files via upload

---
+++ easytask3.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cin >> number;
+	cout << 1;
+	for (int i = 1; i <= number; i++) {
+		if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0)) {
+			if (i % 7 == 0) {
+				i++;
+			}
+			cout << " " << i << " ";
+		}
+	}
+	return 0;
+}
