54ce823ab3e2db24e88fcaaf76ce99d8e1ac9c27
Galya <58034004+Galya777@users.noreply.github.com>
2020-08-06 22:54:38 +0300

    Add files via upload

---
+++ AlgebricProblem2a.cpp
@@ -0,0 +1,57 @@
+﻿//Галя Додова 45616 Информатика първа група 
+#include <iostream>
+using namespace std;
+bool isprime(int p) {
+	if (p < 2) {
+		return false;
+	}	for (int i = 0; i <= p; i++) {
+			if (p % i == 0) {
+				return false;
+			}
+		}
+	return true;
+}
+int findthenumbers(int p=1000) {
+	while (isprime(p) == false) {
+		p--;
+	}
+	if (p % 10 == 3) {
+		for (int i = 0; i < p+2; i++) {
+			for (int j = 2; j < p; j++) {
+				if (isprime(i) == true && isprime(j) == true&&(p+2)%i==0&&(p+2)%j==0) {
+					if (i != 5 && j != 5&& j-i==2) {
+						return p;
+					}
+				}
+			}
+		}
+	}else {
+		p--;
+		findthenumbers(p);
+	}
+
+}
+void proof(int n) {
+	if (n >= 7) {
+		for (int i = n; i < 2 * n; i++) {
+			for (int j = n + 2; i < (2 * n)-2; i++) {
+				if (isprime(i) && isprime(j) && abs(i - j) == 2) {
+					cout << "YES!"<<" ";
+				}
+			}
+		}
+	}
+	else {
+		cout << "NO!" << " ";
+	
+	}
+} //Toва е доказателството на 2б. Има го разписано, но реших да го тествам и като код. 
+int main()
+{
+	cout << findthenumbers(1000)<< " "<<endl;
+	int n;
+	cin >> n;
+	proof(n);
+	return 0;
+}
+
