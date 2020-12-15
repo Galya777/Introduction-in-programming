1557b9af85b79b8f30b62724a89440579c4df24a
Galya <58034004+Galya777@users.noreply.github.com>
2020-12-16 01:03:19 +0200

    Add files via upload
    Напишете програма, която прочита от стандартния вход две цели числа, всяко от които представлява часово време в двоичен формат (Binary clock). За улеснение ще считаме, че последните 6 бита на всяко от числата ще показват стойността на минутите (битове от 0 до 5). Битовете от 6 до 11 - представят часовете. За разлика от минутите, часът се пресмята, по следната схема: стойността, запи    сана в 6, 7, 8 и 9 бит кодира втората цифра от часа, а 10 и 11 бит - първата. При стойност "1" на 12-ти бит, ще считаме времето в 24 часов формат, а при "0" - в 12 часов. След като     е валидирано, пресметнато и отпечатано времето, ще трябва да се намери също и абсолютната разлика между тези две времена в часове и минути. За часовете в 12 часов формат приемете, че са час преди обяд (AM). При невалидно време и/или формат изведете подходящо съобщение.

---
+++ homeworks/homework1_task2_45616.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+void transfer(long long minutes, long long hour){
+    if (minutes >= 60) {
+        hour++;
+        minutes -= 60;
+    }
+}
+long long format(int number, long long hours) {
+    unsigned int mask = 1 << 12;
+
+    if ((number & mask) == 0) {
+        if (hours > 12) {
+            hours -= 12;
+        }
+    }  else {
+        if (hours < 12) {
+            hours +=12;
+        }
+    } if (hours > 24) {
+        hours -= 24;
+    }
+    return hours;
+}
+int main()
+{
+    unsigned int number1, number2;
+    cin >> number1 >> number2;
+    unsigned short num1min =(number1&(1<<6)-1);
+    number1 >> 6;
+    unsigned short num1h = (number1 & (1 << 6) - 1);
+    unsigned short num2min =(number2&(1<<6)-1);
+    number2 >> 6;
+    unsigned short num2h = (number2 & (1 << 6) - 1);
+    num1h = format(number1, num1h);
+    num2h = format(number2, num2h);
+    transfer(num1min, num1h);
+    transfer(num2min, num2h);
+    cout << num1h << ':' << num1min<<endl;
+    cout << num2h << ':' << num2min<<endl;
+    unsigned short diffhours = abs(num1h - num2h);
+    unsigned short diffminutes = abs(num1min - num2min);
+    transfer(diffminutes, diffhours);
+    cout << "dif: " << diffhours << ':';
+    if (diffminutes < 10) {
+        cout << '0';
+    }
+         cout<< diffminutes;
+    return 0;
+}
+
