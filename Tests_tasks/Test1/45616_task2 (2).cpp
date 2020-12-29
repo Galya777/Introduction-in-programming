73a5be358232f40afed64dd60edae76555e43693
Galya <58034004+Galya777@users.noreply.github.com>
2020-12-29 17:19:00 +0200

    Add files via upload

---
+++ Tests_tasks/Test1/45616_task2 (2).cpp
@@ -0,0 +1,56 @@
+// СУ "Св. Климент Охридски"
+// Факултет по математика и информатика
+// Курс Увод в програмирането 2020/21
+// Контролно 1
+// 2020-11-29
+//
+// Начален час: <тук попълнете часа за вашата група>
+// Име: Галя Георгиева Додова
+// ФН: 45616
+// Специалност: Информатика
+// Курс: 1
+// Административна група: 1
+// Използван компилатор: Visual Studio Local Windows Debugger
+#include <iostream>
+using namespace std;
+bool isItPrime(int number) {
+    for (int i = 1; i < number; i++) {
+        if (number % i == 0 && i != 1&&i!=number) {
+            return true;
+        }
+    }
+    return false;
+} int max(int n1, int n2) {
+    if (n1 > n2) {
+        return n1;
+    }
+    else {
+        return n2;
+    }
+} int min(int n1, int n2) {
+    if (n1 < n2) {
+        return n1;
+    }
+    else {
+        return n2;
+    }
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int numberscount = 0;
+    for (int i = min(a, b); i <= max(a, b); i++) {
+        int counterofdivitors = 0;
+        for (int j = 1; j < i; j++) {
+            if (i % j == 0 && j != 1&&j!=i) {
+                counterofdivitors++;
+            }    
+        }
+        if (isItPrime(counterofdivitors) == true) {
+            numberscount++;
+        }
+    }
+    cout << numberscount;
+    return 0;
+}
